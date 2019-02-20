32b19c170fe025f5dc81a2dcd41d883876e0ba86
liuxiao <liuxiao@liuxiao-mac-mini.local>
2019-02-20 10:46:40 +0800

    [update][gralde] ConstraintLayout升级到2.0

---
+++ app/src/main/java/com/xiaoshao/andes/working/MotionLayoutFragment.java
@@ -0,0 +1,14 @@
+package com.xiaoshao.andes.working;
+
+import com.xiaoshao.andes.base.BaseFragment;
+
+/**
+ * Description: ConstraintLayout2.0发布beta/正式版后继续，目前测试版IDE酷炫功能不开放
+ * Author: liuxiao
+ * Date: 2019/2/19
+ */
+public class MotionLayoutFragment extends BaseFragment {
+
+
+
+}
